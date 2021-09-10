6256cce7c5ffba2c35273b39a97b84c62fe40ea5
Samarth Upadhyaya <samarthupadhyaya@Samarths-MacBook-Pro.local>
2021-09-11 05:54:00 +0800

    Added react-native navigation library

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import AvailableDevicesScreen from './screens/AvailableDevices.js'
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AvailableDevicesScreen />
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ screens/AvailableDevices.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'
+
+const AvailableDevicesScreen = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  )
+}
+
+const styles = Stylesheet.create({
+    
+})
+
+export default AvailableDevicesScreen
